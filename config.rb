8dcbb7d735c7d9e60f780409269fb188eb94c6a7
Francis Marineau <frank.marineau@gmail.com>
2015-12-21 14:46:31 -0500

    Wire prev/next product nav

--- config.rb
+++ config.rb
@@ -10,6 +10,26 @@ after_configuration do
 	sprockets.append_path File.join "#{root}", @bower_config["directory"]
 end
 
+helpers do
+  def product_index(product_title)
+    products = data.menuElements.products
+    product = products.detect{ |p| p.title == product_title }
+    products.index product
+  end
+
+  def next_product_path
+    next_product_index = product_index(current_page.data.title) + 1
+    next_product_index = 0 if next_product_index > data.menuElements.products.length - 1
+    data.menuElements.products[next_product_index].href
+  end
+
+  def prev_product_path
+    prev_product_index = product_index(current_page.data.title) - 1
+    prev_product_index = data.menuElements.products.length - 1 if prev_product_index < 0
+    data.menuElements.products[prev_product_index].href
+  end
+end
+
 activate :livereload
 
 activate :directory_indexes
