a6de299922228789f741aae3c52bc303711ab781
Alexander Haniotis <alexanderhaniotis@gmail.com>
2015-12-20 18:04:59 -0800

    Remove fixed nav

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -34,29 +34,10 @@ $('input[type="range"]').rangeslider({
 });
 
 
-// Fixed nav
-
-$(window).scroll(function() {    
-  var scroll = $(window).scrollTop();
-  if (scroll >= 400) {
-    $(".fixed-nav, .fixed-product-nav").addClass("visible");
-  } else {
-    $(".fixed-nav, .fixed-product-nav").removeClass("visible");
-  }
-});
-
-
 // Product nav
 
 $(".product-nav-trigger").click(function() {
   $("body").toggleClass("js-product-nav-open");
-});
-
-
-// Fixed product nav (scrolled)
-
-$(".fixed-product-nav-trigger").click(function() {
-  $("body").toggleClass("js-fixed-product-nav-open");
 });
 
 
@@ -71,23 +52,10 @@ $(".mobile-nav .btn").click(function() {
 });
 
 
-// Set offset for smooth scrolling
+// Smooth scroll
 
-var mq = window.matchMedia( "(min-width: 1140px)" );
-var menuHeight = $('.fixed-nav').eq(0).outerHeight();
-
-if (mq.matches) {
-  $("a").smoothScroll({
-    easing: "swing",
-    speed: 300,
-    offset: -menuHeight + 2 // Compensate for 1px difference
-  });
-}
-
-else {
-  $("a").smoothScroll({
-    easing: "swing",
-    speed: 300,
-    offset: 2
-  });
-}
+$("a").smoothScroll({
+  easing: "swing",
+  speed: 300,
+  offset: 2
+});
